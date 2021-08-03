e78a90413c85a7615c8e3067e9633c49d0f16ec8
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2021-08-03 11:42:38 -0700

    Add reference to BlockchainAdapter

--- packages/ethereum/src/EthereumAdapter.ts
+++ packages/ethereum/src/EthereumAdapter.ts
@@ -1,1 +1,7 @@
-export default class EthereumAdapter {}
+import { BlockchainAdapter } from '@parse/blockchain';
+
+export default class EthereumAdapter implements BlockchainAdapter {
+  send(parseObjectFullJSON: Record<string, unknown>): void {
+    throw new Error('Method not implemented.');
+  }
+}
