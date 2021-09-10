ed98c8fb22a069beda7c0e8ee45591c3c7a62a8c
Antonio Davi Macedo Coelho de Castro <adavimacedo@gmail.com>
2021-09-09 17:08:48 -0700

    Add App Owners feature (#2)

--- packages/ethereum/contracts/Parse.sol
+++ packages/ethereum/contracts/Parse.sol
@@ -3,17 +3,30 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract Parse {
   struct Class {
+    string name;
     string[] objectsIds;
     mapping(string => string) objectJSONById;
   }
 
   struct App {
+    string id;
+    mapping (address => bool) owners;
     string[] classesNames;
     mapping(string => Class) classByName;
   }
 
   event AppCreated (
     string _appId
+  );
+
+  event AppOwnerAdded (
+    string _appId,
+    address _owner
+  );
+
+  event AppOwnerRemoved (
+    string _appId,
+    address _owner
   );
 
   event ClassCreated (
@@ -32,19 +45,53 @@ contract Parse {
   string[] appsIds;
   mapping(string => App) appById;
 
+  function addAppOwner(string memory _appId, address _owner) public {
+    require(bytes(_appId).length > 0, "_appId is required");
+    App storage app = appById[_appId];
+    require(
+      msg.sender == owner || app.owners[msg.sender],
+      "This function is restricted to the contract and app owners"
+    );
+    require(!app.owners[_owner], "The address is already an app owner");
+    if (bytes(app.id).length <= 0) {
+      app.id = _appId;
+      appsIds.push(_appId);
+      emit AppCreated(_appId);
+    }
+    app.owners[_owner] = true;
+    emit AppOwnerAdded(_appId, _owner);
+  }
+
+  function removeAppOwner(string memory _appId, address _owner) public {
+    require(bytes(_appId).length > 0, "_appId is required");
+    App storage app = appById[_appId];
+    require(
+      msg.sender == owner || app.owners[msg.sender],
+      "This function is restricted to the contract and app owners"
+    );
+    require(app.owners[_owner], "The address is not an app owner");
+    app.owners[_owner] = false;
+    emit AppOwnerRemoved(_appId, _owner);
+  }
+
   function createObject(string memory _appId, string memory _className, string memory _objectId, string memory _objectJSON) public {
-    require(msg.sender == owner, "This function is restricted to the contract's owner");
     require(bytes(_appId).length > 0, "_appId is required");
     require(bytes(_className).length > 0, "_className is required");
     require(bytes(_objectId).length > 0, "_objectId is required");
     require(bytes(_objectJSON).length > 0, "_objectJSON is required");
     App storage app = appById[_appId];
-    if (app.classesNames.length <= 0) {
+    require(
+      msg.sender == owner || app.owners[msg.sender],
+      "This function is restricted to the contract and app owners"
+    );
+    if (bytes(app.id).length <= 0) {
+      app.id = _appId;
       appsIds.push(_appId);
       emit AppCreated(_appId);
     }
     Class storage class = app.classByName[_className];
-    if (class.objectsIds.length <= 0) {
+    if (bytes(class.name).length <= 0) {
+      class.name = _className;
       app.classesNames.push(_className);
       emit ClassCreated(_appId, _className);
     }
