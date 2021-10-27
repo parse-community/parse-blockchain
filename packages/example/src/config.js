ead6fe6f2a3bf54d4c645ea3aa166433d8a16e32
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2021-10-27 22:17:38 +0200

    Update config.js

--- packages/example/src/config.js
+++ packages/example/src/config.js
@@ -1,13 +1,5 @@
 module.exports = {
   /**
-   * The following values do not need to be modified for this example to work.
-   */
-  // The names of the classes whose objects you want to send to blockchain
-  classNames: ['SomeBlockchainClass'],
-  // The blockchain network ID; same as used for ganache in script `npm run prepare:network`
-  networkId: '1000000000000',
-
-  /**
    * The following values need to be modified for this example to work.
    */
   // Paste a public account address after running `npm run prepare:network`
@@ -16,4 +8,14 @@ module.exports = {
   accountPrivateKey: 'ACCOUNT_PRIVATE_KEY',
   // Paste the Parse contract address after running `npm run prepare:contract`
   contractAddress: 'CONTRACT_ADDRESS',
+
+  /**
+   * The following values do not need to be modified for this example to work.
+   * If you do modify them, check whether they are hardcoded somewhere in
+   * this package's npm scripts and modify them there as well.
+   */
+  // The names of the classes whose objects you want to send to blockchain
+  classNames: ['SomeBlockchainClass'],
+  // The blockchain network ID; same as used for ganache in script `npm run prepare:network`
+  networkId: '1000000000000',
 };
