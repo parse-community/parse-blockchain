3f4afd353277cfc02d94c0f6e06fb2f8612f2b40
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2021-10-27 19:58:04 +0200

    add truffle config

---
+++ packages/example/truffle-config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = {
+  contracts_directory: path.resolve(__dirname, './node_modules/@parse/ethereum/contracts'),
+  contracts_build_directory: path.resolve(__dirname, './node_modules/@parse/ethereum/build/contracts'),
+  migrations_directory: path.resolve(__dirname, './node_modules/@parse/ethereum/migrations'),
+  networks: {
+    parseserverblockchaindev: {
+      provider: () =>
+        new HDWalletProvider(
+          'THE ACCOUNT PRIVATE KEY', // Copy to here the private key of one of your Ganache auto-generated accounts
+          'ws://127.0.0.1:8545'
+        ),
+      network_id: '1000000000000', // The same network id that you used on Ganache
+      from: 'THE ACCOUNT ADDRESS', // Copy to here the address of one of your Ganache auto-generated accounts
+    },
+  },
+};
