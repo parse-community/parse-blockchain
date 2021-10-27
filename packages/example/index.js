410898c2da1d77cf2760feb939690617d5fa0436
Manuel Trezza <5673677+mtrezza@users.noreply.github.com>
2021-10-27 19:52:23 +0200

    add example repo

---
+++ packages/example/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const { default: ParseServer } = require('parse-server');
+const { SimpleMQAdapter, bridge, worker } = require('@parse/blockchain');
+const { EthereumAdapter } = require('@parse/ethereum');
+const Web3 = require('web3');
+
+const app = express();
+
+const parseServer = new ParseServer({
+  serverURL: 'http://localhost:1337/parse',
+  appId: 'someappid',
+  masterKey: 'somemasterkey',
+  databaseURI: 'mongodb://localhost:27017/parseserverblockchaindev',
+});
+
+const mqAdapter = new SimpleMQAdapter();
+
+const web3 = new Web3('ws://127.0.0.1:8545');
+web3.eth.accounts.wallet.add(
+   'THE ACCOUNT PRIVATE KEY', // Copy to here the private key that you used to deploy the contracts
+);
+
+bridge.initialize(
+  ['SomeBlockchainClass'], // Pass here the name of the classes whose objects you want to send to blockchain
+  mqAdapter
+);
+worker.initialize(
+  new EthereumAdapter(
+    web3,
+    'THE CONTRACT ADDRESS', // Copy to here the Parse contract address that you copied after deploying it
+    'THE ACCOUNT ADDRESS', // Copy to here the address that you used to deploy the contracts
+  ),
+  mqAdapter
+);
+
+app.use('/parse', parseServer.app);
+
+app.listen(1337, () => {
+  console.log('REST API running on http://localhost:1337/parse');
+});
